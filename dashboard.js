f76978de113ff5be0eef9b7a657524867d1018f3
JojiK85 <kandulajoji@gmail.com>
2025-12-16 22:19:55 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -22,7 +22,7 @@ const auth = getAuth(app);
 
 /* ================= BACKEND ================= */
 const API =
-  "https://script.google.com/macros/s/AKfycbxTtdt1HmTIP0i5WOeXMLRWLKA1k4RpD153EmgU-Ow6CPRGOISzjOVLplKFDm-gUaggmg/exec";
+  "https://script.google.com/macros/s/AKfycbzTEBghmv7_zmf62sIq6ov64reyOIC6mWLfl_Rc3ZPThO05ccQYhIrJ48lrSYcRlT-PTA/exec";
 
 /* ================= DOM ================= */
 const adminEmailEl = document.getElementById("adminEmail");
