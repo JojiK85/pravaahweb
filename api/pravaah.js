8bd58dbe3fc324326da294438c5256293ccfcea3
JojiK85 <kandulajoji@gmail.com>
2025-12-17 12:08:47 +0530

    Update pravaah.js

--- api/pravaah.js
+++ api/pravaah.js
@@ -1,19 +1,22 @@
 export default async function handler(req, res) {
   try {
-    const qs = new URLSearchParams(req.query).toString();
-
     const GAS_URL =
       "https://script.google.com/macros/s/AKfycbyAiWPJSE7Qm3qPyL-vZTlBR07xlhqzSRxfu-eeXMnXowOE8GXc1AZsLGW1ZOwyP7kjow/exec";
 
-    const url = qs ? `${GAS_URL}?${qs}` : GAS_URL;
+    let url = GAS_URL;
+
+    if (req.method === "GET") {
+      const qs = new URLSearchParams(req.query).toString();
+      if (qs) url += "?" + qs;
+    }
 
     const gasRes = await fetch(url, {
       method: req.method,
-      headers: { "Content-Type": "application/json" }
+      headers: { "Content-Type": "application/json" },
+      body: req.method === "POST" ? JSON.stringify(req.body) : undefined
     });
 
     const text = await gasRes.text();
-
     res.status(200).send(text);
 
   } catch (e) {
